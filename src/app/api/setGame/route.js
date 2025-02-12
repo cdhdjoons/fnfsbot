613a1a824649b76bce5eba225358c8a966a65e97
DevCrypto187 <crypto_dev@marzmain.io>
2025-02-13 04:06:08 +0900

    fnfs setgame change

--- src/app/api/setGame/route.js
+++ src/app/api/setGame/route.js
@@ -1,25 +1,33 @@
-// app/api/setGame/route.js
-import fetch from 'node-fetch';
+import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function POST(req) {
   try {
+    const body = await req.json();
+    
+    const gameShortName = 'fnfscrazycar';  // Telegram Bot에 설정된 short name
+    const gameUrl = 'https://fnfsgame.vercel.app/';  // 실제 게임 URL
+
     const response = await fetch(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/setGameShortName`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        game_short_name: 'fnfscrazycar',  // 게임의 short name
-        game_url: 'https://fnfsgame.vercel.app/',  // 실제 게임 URL
+        game_short_name: gameShortName,  // 게임의 short name
+        game_url: gameUrl,  // 실제 게임 URL
       }),
     });
 
     const data = await response.json();
-    console.log('Game set response:', data);
 
-    return new Response('Game URL set successfully', { status: 200 });
+    if (data.ok) {
+      return NextResponse.json({ message: 'Game successfully set.' });
+    } else {
+      throw new Error('Failed to set game');
+    }
   } catch (error) {
     console.error('Error setting game:', error);
-    return new Response('Error setting game', { status: 500 });
+    return NextResponse.json({ message: 'Error setting game', error: error.message }, { status: 500 });
   }
 }
+
